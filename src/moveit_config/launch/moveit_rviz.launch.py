a062bd489b3f7b66134e8ed226a7683c3d9a23af
JintaoChan <JintaoChan@todo.com>
2025-04-29 01:02:21 +0800

    UPDATE: moveit works!

--- src/moveit_config/launch/moveit_rviz.launch.py
+++ src/moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("TechControlInterface", package_name="moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("moveit_config", package_name="moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
